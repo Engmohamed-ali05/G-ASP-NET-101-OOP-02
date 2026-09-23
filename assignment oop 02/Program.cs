4b8f6459427f37aa380d3113152872880f55df56
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-23 16:55:27 +0300

    Add project files.

---
+++ assignment oop 02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace assignment_oop_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
