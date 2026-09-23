ecd45e2abb1bec7b2d5c772335b0b666fceb3a00
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-23 17:26:21 +0300

    question 2

--- assignment oop 02/Program.cs
+++ assignment oop 02/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace assignment_oop_02
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment_oop_02
 {
     internal class Program
     {
@@ -7,16 +10,32 @@
 
 
             #region  Question 1
-        // A -    What is the difference between a class and a struct?
+            // A -    What is the difference between a class and a struct?
 
-            *  Class: Reference type, supports inheritance.
-                   
-               * Struct: Value type, doesn't support inheritance
-              
-             *******************************************************************************************
+            //  *  Class: Reference type, supports inheritance.
+
+            //   * Struct: Value type, doesn't support inheritance
+
+            //*******************************************************************************************
             //B - Why are classes more suitable than structs for large applications?
 
-            =>  Classes are better for large applications because they support inheritance, encapsulation, and polymorphism.
+            //=>  Classes are better for large applications because they support inheritance, encapsulation, and polymorphism.
+
+            #endregion
+
+            #region  Question 2
+            // A -Which class is the parent class?
+            Shipment is the parent 
+            ***************************************
+            // B -Which class is the child class?
+            ExpressShipment is the chil
+            ***********************************************
+            //c-What members are inherited by ExpressShipment?
+            TrackingCode
+            ************************************************
+            // D- Why is inheritance better than duplicating the same code?
+             avoids code duplication and makes the code easier to maintain.
+
 
             #endregion
 
@@ -25,15 +44,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
         }
-}
+    }
 }
