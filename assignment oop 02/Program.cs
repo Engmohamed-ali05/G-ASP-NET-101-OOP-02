754b2c11d14eb77ec7c38a140d5c33aa5e6331a9
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-23 18:04:30 +0300

    question 4 part 2

--- assignment oop 02/Program.cs
+++ assignment oop 02/Program.cs
@@ -277,121 +277,122 @@ public class Shipment
 
             #region Question 3 part 2
             // q 3 
-
-public class DeliveryCenter
-        {
-            public string CenterName { get; set; }
-
-            private Shipment[] shipments;
-
-            private int count;
-
-            public DeliveryCenter(string centerName)
-            {
-                CenterName = centerName;
-                shipments = new Shipment[20];
-                count = 0;
-            }
-
-           
-            public bool AddShipment(Shipment shipment)
-            {
-                if (shipment == null)
-                {
-                    return false;
-                }
-
-                if (count >= 20)
-                {
-                    return false;
-                }
-
-                shipments[count] = shipment;
-                count++;
-
-                return true;
-            }
-
-            public Shipment this[int index]
-            {
-                get
-                {
-                    if (index < 0 || index >= count)
+            /*
+            public class DeliveryCenter
                     {
-                        throw new IndexOutOfRangeException();
-                    }
+                        public string CenterName { get; set; }
 
-                    return shipments[index];
-                }
+                        private Shipment[] shipments;
 
-                set
-                {
-                    if (index < 0 || index >= count)
-                    {
-                        throw new IndexOutOfRangeException();
-                    }
+                        private int count;
 
-                    shipments[index] = value;
-                }
-            }
-
-            
-            public Shipment this[string trackingCode]
-            {
-                get
-                {
-                    for (int i = 0; i < count; i++)
-                    {
-                        if (shipments[i].TrackingCode == trackingCode)
+                        public DeliveryCenter(string centerName)
                         {
-                            return shipments[i];
+                            CenterName = centerName;
+                            shipments = new Shipment[20];
+                            count = 0;
+                        }
+
+
+                        public bool AddShipment(Shipment shipment)
+                        {
+                            if (shipment == null)
+                            {
+                                return false;
+                            }
+
+                            if (count >= 20)
+                            {
+                                return false;
+                            }
+
+                            shipments[count] = shipment;
+                            count++;
+
+                            return true;
+                        }
+
+                        public Shipment this[int index]
+                        {
+                            get
+                            {
+                                if (index < 0 || index >= count)
+                                {
+                                    throw new IndexOutOfRangeException();
+                                }
+
+                                return shipments[index];
+                            }
+
+                            set
+                            {
+                                if (index < 0 || index >= count)
+                                {
+                                    throw new IndexOutOfRangeException();
+                                }
+
+                                shipments[index] = value;
+                            }
+                        }
+
+
+                        public Shipment this[string trackingCode]
+                        {
+                            get
+                            {
+                                for (int i = 0; i < count; i++)
+                                {
+                                    if (shipments[i].TrackingCode == trackingCode)
+                                    {
+                                        return shipments[i];
+                                    }
+                                }
+
+                                return null;
+                            }
+                        }
+
+                        public bool RemoveShipment(string trackingCode)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                if (shipments[i].TrackingCode == trackingCode)
+                                {
+
+                                    for (int j = i; j < count - 1; j++)
+                                    {
+                                        shipments[j] = shipments[j + 1];
+                                    }
+
+                                    shipments[count - 1] = null;
+                                    count--;
+
+                                    return true;
+                                }
+                            }
+
+                            return false;
+                        }
+
+
+                        public void PrintAllShipments()
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"===== {CenterName} =====");
+
+                            if (count == 0)
+                            {
+                                Console.WriteLine("No shipments available.");
+                                return;
+                            }
+
+                            for (int i = 0; i < count; i++)
+                            {
+                                shipments[i].PrintShipment();
+                            }
                         }
                     }
 
-                    return null;
-                }
-            }
-
-            public bool RemoveShipment(string trackingCode)
-            {
-                for (int i = 0; i < count; i++)
-                {
-                    if (shipments[i].TrackingCode == trackingCode)
-                    {
-                        
-                        for (int j = i; j < count - 1; j++)
-                        {
-                            shipments[j] = shipments[j + 1];
-                        }
-
-                        shipments[count - 1] = null;
-                        count--;
-
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-          
-            public void PrintAllShipments()
-            {
-                Console.WriteLine();
-                Console.WriteLine($"===== {CenterName} =====");
-
-                if (count == 0)
-                {
-                    Console.WriteLine("No shipments available.");
-                    return;
-                }
-
-                for (int i = 0; i < count; i++)
-                {
-                    shipments[i].PrintShipment();
-                }
-            }
-        }
 
 
 
@@ -408,13 +409,201 @@ public class DeliveryCenter
 
 
 
-
-
+                        */
 
 
             #endregion
 
+            #region Question 4 part 2
 
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("*********************************");
+                Console.WriteLine(" Smart Delivery Management System");
+                Console.WriteLine("***********************************");
+
+                
+                Console.Write("Enter Center Name: ");
+                string centerName = Console.ReadLine();
+
+                DeliveryCenter center = new DeliveryCenter(centerName);
+
+             
+
+                Console.WriteLine();
+                Console.WriteLine("=== Standard Shipment ===");
+
+                Console.Write("Tracking Code: ");
+                string trackingCode1 = Console.ReadLine();
+
+                Console.Write("Description: ");
+                string description1 = Console.ReadLine();
+
+                Console.Write("Weight: ");
+                decimal weight1 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Delivery Fee: ");
+                decimal fee1 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("City: ");
+                string city1 = Console.ReadLine();
+
+                Console.Write("Street: ");
+                string street1 = Console.ReadLine();
+
+                DeliveryAddress address1 =
+                    new DeliveryAddress(city1, street1);
+
+                StandardShipment standard =
+                    new StandardShipment(
+                        trackingCode1,
+                        description1,
+                        weight1,
+                        fee1,
+                        address1);
+
+                
+
+                Console.WriteLine();
+                Console.WriteLine("=== Express Shipment ===");
+
+                Console.Write("Tracking Code: ");
+                string trackingCode2 = Console.ReadLine();
+
+                Console.Write("Description: ");
+                string description2 = Console.ReadLine();
+
+                Console.Write("Weight: ");
+                decimal weight2 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Delivery Fee: ");
+                decimal fee2 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("City: ");
+                string city2 = Console.ReadLine();
+
+                Console.Write("Street: ");
+                string street2 = Console.ReadLine();
+
+                Console.Write("Extra Fee: ");
+                decimal extraFee = decimal.Parse(Console.ReadLine());
+
+                DeliveryAddress address2 =
+                    new DeliveryAddress(city2, street2);
+
+                ExpressShipment express =
+                    new ExpressShipment(
+                        trackingCode2,
+                        description2,
+                        weight2,
+                        fee2,
+                        address2,
+                        extraFee);
+
+
+                Console.WriteLine();
+                Console.WriteLine("=== International Shipment ===");
+
+                Console.Write("Tracking Code: ");
+                string trackingCode3 = Console.ReadLine();
+
+                Console.Write("Description: ");
+                string description3 = Console.ReadLine();
+
+                Console.Write("Weight: ");
+                decimal weight3 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Delivery Fee: ");
+                decimal fee3 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("City: ");
+                string city3 = Console.ReadLine();
+
+                Console.Write("Street: ");
+                string street3 = Console.ReadLine();
+
+                Console.Write("Destination Country: ");
+                string country = Console.ReadLine();
+
+                Console.Write("Customs Fee: ");
+                decimal customsFee = decimal.Parse(Console.ReadLine());
+
+                DeliveryAddress address3 =
+                    new DeliveryAddress(city3, street3);
+
+                InternationalShipment international =
+                    new InternationalShipment(
+                        trackingCode3,
+                        description3,
+                        weight3,
+                        fee3,
+                        address3,
+                        country,
+                        customsFee);
+
+              
+
+                center.AddShipment(standard);
+                center.AddShipment(express);
+                center.AddShipment(international);
+
+                
+                center.PrintAllShipments();
+
+              
+
+                Console.WriteLine();
+                Console.WriteLine("=== Search Shipment ===");
+
+                Console.Write("Enter Tracking Code: ");
+                string searchCode = Console.ReadLine();
+
+                Shipment foundShipment = center[searchCode];
+
+                if (foundShipment != null)
+                {
+                    Console.WriteLine("Shipment Found:");
+                    foundShipment.PrintShipment();
+                }
+                else
+                {
+                    Console.WriteLine("Shipment not found.");
+                }
+
+                
+
+                Console.WriteLine();
+                Console.WriteLine("=== Remove Shipment ===");
+
+                Console.Write("Enter Tracking Code to remove: ");
+                string removeCode = Console.ReadLine();
+
+                bool removed = center.RemoveShipment(removeCode);
+
+                if (removed)
+                {
+                    Console.WriteLine("Shipment removed successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Shipment not found.");
+                }
+
+             
+
+                Console.WriteLine();
+                Console.WriteLine("=== Remaining Shipments ===");
+
+                center.PrintAllShipments();
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+            #endregion
 
 
     }
